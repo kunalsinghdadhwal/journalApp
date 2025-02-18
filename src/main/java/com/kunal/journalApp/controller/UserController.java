05305690360b56541539549444a12b4c91fb55b5
Hug_Bunter <kunalsinghdadhwal@gmail.com>
2025-02-18 11:07:26 +0530

    User Setup

---
+++ src/main/java/com/kunal/journalApp/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.kunal.journalApp.controller;
+
+import com.kunal.journalApp.entity.User;
+import com.kunal.journalApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getallUsers() {
+        return userService.getAll();
+    }
+
+    @PostMapping
+    public void createUser(@RequestBody User user){
+        userService.saveUser(user);
+    }
+
+    @PutMapping("/{userName}")
+    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable String userName){
+        User userInDb = userService.findByUserName(userName);
+        if(userInDb != null){
+            userInDb.setUserName(user.getUserName());
+            userInDb.setPassword(user.getPassword());
+            userService.saveUser(userInDb);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
